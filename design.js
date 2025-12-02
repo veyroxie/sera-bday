b6265f5d64db97a23d48956270ecbb1652e04255
elyte03 <etee0005@student.monash.edu>
2025-12-02 17:30:31 +0800

    initialised files from jsfiddle.net

---
+++ design.js
@@ -0,0 +1,15 @@
+const playBtn = document.getElementById("playSongBtn");
+    const loveNoteBtn = document.getElementById("loveNoteBtn");
+    const loveNote = document.getElementById("loveNote");
+
+    playBtn.addEventListener("click", () => {
+      document.querySelector(".spotify-embed").scrollIntoView({ behavior: "smooth" });
+      playBtn.innerHTML = '<span class="icon">🎧</span> Now playing: you + this song';
+    });
+
+    loveNoteBtn.addEventListener("click", () => {
+      loveNote.classList.toggle("visible");
+      loveNoteBtn.innerHTML = loveNote.classList.contains("visible")
+        ? '<span class="icon">😉</span> Hide the soft stuff'
+        : '<span class="icon">💌</span> Click for a slightly illegal level of flirt';
+    });
